080caddef8efae8da2ef4f0e807cebdc876321c1
David <d3fung@gmail.com>
2012-06-15 14:40:02 -0400

    add back blinds

--- animation.js
+++ animation.js
@@ -62,13 +62,15 @@ vp.intro.zoom = function(fnRender, ctx, cX, cY, width, height)
 
 vp.intro.flyIn = function(fnRender, ctx, cX, cY, width, height)
 {
+    var dest = 20;
+
     ctx.save();
     ctx.translate(cX, cY);
 
-    if (vp.intro.timeIndex < 50)
+    if (vp.intro.timeIndex < dest)
     {
-        ctx.globalAlpha = (vp.intro.timeIndex - 1) / 50;
-        ctx.scale(50 - vp.intro.timeIndex, 50 - vp.intro.timeIndex);
+        ctx.globalAlpha = (vp.intro.timeIndex - 1) / dest;
+        ctx.scale(dest - vp.intro.timeIndex, dest - vp.intro.timeIndex);
     }
 
     ctx.translate(-width / 2, height / 2);
@@ -89,3 +91,53 @@ vp.intro.fadeIn = function(fnRender, ctx, cX, cY, width, height)
 };
 
 
+
+vp.intro.Blinds = function(fnRender, ctx, cX, cY, width, height)
+{
+    var me = this;
+    var fnShuffle = function(o)
+    {
+        for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o
+    };
+
+    var blindHeight = 4;
+    var order = [];
+    for (var i = 1; i <= (height * 1.2); i++)
+    {
+        if (i % blindHeight == 1)
+        {
+            order.push(i);
+        }
+    }
+    order = fnShuffle(order);
+
+    var visible = [];
+
+    this.step = function()
+    {
+
+        ctx.save();
+        ctx.translate(cX, cY)
+
+        if (order.length != 0)
+        {
+            visible.push(order.pop());
+
+            ctx.beginPath();
+            for (var i = 0; i < visible.length; i++)
+            {
+                ctx.rect(0, visible[i], width, blindHeight);
+            }
+            ctx.clip();
+        }
+
+        ctx.translate(0, height);
+
+        fnRender();
+
+        ctx.translate(0, -height);
+
+        ctx.restore();
+    };
+};
