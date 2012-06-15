85f5dbf9b3984781dbc9ec539e72aa8e6bedf3c6
Diana Huang <dhuang@vistaprint.com>
2012-06-15 15:24:28 -0400

    clean up rendering and add canvas to config

---
+++ rendering.js
@@ -0,0 +1,34 @@
+if(!window.vp)
+{
+    vp = function(){};
+}
+
+if(!vp.intro)
+{
+    vp.intro = function(){};
+}
+
+
+vp.intro.addImage = function(ctx, imgSrc, x, y)
+{
+	ctx.save();
+	var img = new Image();
+	img.src = imgSrc;
+	ctx.translate(x, y);
+	ctx.drawImage(img, 0, -img.height);
+	ctx.restore();
+};
+
+vp.intro.addText = function(ctx, sText, x, y)
+{
+	ctx.save();
+        ctx.font = "20px Chango";
+        ctx.fillStyle = "Black";
+	ctx.translate(x, y);
+        var textWidth = ctx.measureText(sText);
+        ctx.fillText(sText, 0, 0);
+	ctx.restore();
+};
+
+
+
